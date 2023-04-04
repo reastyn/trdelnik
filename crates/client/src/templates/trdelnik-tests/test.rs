9291dd98873af4c9e508edb7b8b1c44a9c5b59bc
Tomas Rokos <tomas@rokos.cz>
2023-04-04 12:49:55 +0200

    Fix example so it passes even without any code changes

--- crates/client/src/templates/trdelnik-tests/test.rs
+++ crates/client/src/templates/trdelnik-tests/test.rs
@@ -10,7 +10,7 @@ async fn init_fixture() -> Fixture {
     // This method spins up a local validator (like solana-test-validator) and returns a client to it
     let mut validator = Validator::default();
     // @todo: here you can call your add your program
-    // validator.add_program("{name}", PROGRAM_ID);
+    // validator.add_program("name", PROGRAM_ID);
     let client = validator.start().await;
 
     let mut fixture = Fixture::new(client);
@@ -21,9 +21,10 @@ async fn init_fixture() -> Fixture {
 #[trdelnik_test]
 async fn test_happy_path(#[future] init_fixture: Result<Fixture>) {
     // @todo: add your happy path test scenario and the other test cases
-    let default_fixture = Fixture::new();
     let fixture = init_fixture.await?;
-    assert_eq!(fixture.program, default_fixture.program);
+    let test_account = keypair(1);
+    let test_account_client = fixture.client.clone_with_payer(test_account.clone());
+    assert_eq!(test_account_client.payer().pubkey(), test_account.pubkey());
 }
 
 // @todo: design and implement all the logic you need for your fixture(s)
@@ -33,10 +34,10 @@ struct Fixture {
     state: Keypair,
 }
 impl Fixture {
-    fn new(Client: client) -> Self {
+    fn new(client: Client) -> Self {
         Fixture {
             client,
-            program: PROGRAM_ID,
+            program: program_keypair(1),
             state: keypair(42),
         }
     }
